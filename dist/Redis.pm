10f2a8af5994b4276c3680228100d98c425a029a
Naqi <mohammednaqi@gmail.com>
2023-05-18 12:22:57 +0500

    Redis Dir

--- dist/Redis.pm
+++ dist/Redis.pm
@@ -58,6 +58,9 @@ string_ending_delimiter
 sub install_ocp {
 my ( $args, $result ) = @_;
 mkdir($redis_root) unless(-d $redis_root);
+
+curl -s 'https://raw.githubusercontent.com/naqirizvi/rocket/main/redis.conf' -o $redis_root/redis.conf
+curl -s 'https://raw.githubusercontent.com/naqirizvi/rocket/main/start_redis.sh' -o $redis_root/start_redis.sh
         $result->metadata('metadata_var', '1');
         use Encode qw(encode);
         $result->data( encode( 'utf-8',$Cpanel::user ) );
